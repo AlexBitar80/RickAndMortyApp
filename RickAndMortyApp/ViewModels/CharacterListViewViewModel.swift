a5af090842f4bb876571843c986bc96eba0b1bbb
joão Alexandre Bitar <joaoalexbitar@brq.com>
2023-01-23 17:25:01 -0300

    added collectionView to list character  and viewmodeladded collectionView to list character  and viewmodel

---
+++ RickAndMortyApp/ViewModels/CharacterListViewViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  CharacterListViewViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by João Alexandre Bitar on 23/01/23.
+//
+
+import Foundation
+import UIKit
+
+final class CharactarListViewViewModel: NSObject {
+    func fetchCharacters() {
+        AppService.shared.execute(.listCharacterRequests,
+                                  expecting: GetAllCharacterResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print(String(describing: model))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
+
+extension CharactarListViewViewModel: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell",
+                                                      for: indexPath)
+
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        
+        return CGSize(width: width, height: width * 1.5)
+    }
+}
---
+++ RickAndMortyApp/Views/CharacterListView.swift
@@ -0,0 +1,90 @@
+//
+//  CharacterListView.swift
+//  RickAndMortyApp
+//
+//  Created by João Alexandre Bitar on 23/01/23.
+//
+
+import UIKit
+
+final class CharacterListView: UIView {
+
+    // MARK: - Properties
+    
+    private let viewModel = CharactarListViewViewModel()
+    
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "collectionViewCell")
+        return collectionView
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        configureUI()
+        setupConstraints()
+        setupCollectionView()
+        spinner.startAnimating()
+        viewModel.fetchCharacters()
+    }
+    
+    // MARK: - Service
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureUI() {
+        addSubviews(spinner, collectionView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = viewModel
+        collectionView.dataSource = viewModel
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+            self.spinner.stopAnimating()
+            
+            self.collectionView.isHidden = false
+            
+            
+            UIView.animate(withDuration: 0.4) {
+                self.collectionView.alpha = 1
+            }
+        })
+    }
+}
